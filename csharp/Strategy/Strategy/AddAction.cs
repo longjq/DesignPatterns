ff7209aca36e3f2b7f830a812ef3f047b9a45e38
longjq <87399497@qq.com>
2018-06-18 22:57:23 +0800

    add csharp strategy

---
+++ csharp/Strategy/Strategy/AddAction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class AddAction : Action
+    {
+        string Action.doAction()
+        {
+            return "Add Action";
+        }
+    }
+}
---
+++ csharp/Strategy/Strategy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Context ctx = new Context(new AddAction());
+            Console.WriteLine(ctx.action());
+
+            Console.Read();
+        }
+    }
+}
