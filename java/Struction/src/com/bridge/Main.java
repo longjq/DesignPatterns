c068fd229423e8ccae7ba1cd1c5a457fc80d138c
longjq <87399497@qq.com>
2021-02-13 09:29:10 +0800

    add:brige

--- java/Struction/src/com/bridge/Main.java
+++ java/Struction/src/com/bridge/Main.java
@@ -2,8 +2,39 @@ package com.bridge;
 
 public class Main {
     public static void main(String[] args) {
-
+        Implementor imple = new ConcreteImplementorA();
+        Abstraction abs = new RefinedAbstractionImpl(imple);
+        abs.Operation();
     }
 }
 
+interface Implementor{
+    void OperationImpl();
+}
 
+class ConcreteImplementorA implements Implementor{
+    @Override
+    public void OperationImpl() {
+        System.out.println("具体实现（Concrete Implementor）角色被访问");
+    }
+}
+abstract class Abstraction{
+    protected Implementor imple;
+
+    protected Abstraction(Implementor imple){
+        this.imple = imple;
+    }
+
+    protected abstract void Operation();
+}
+class RefinedAbstractionImpl extends Abstraction {
+    protected RefinedAbstractionImpl(Implementor imple) {
+        super(imple);
+    }
+
+    @Override
+    protected void Operation() {
+        System.out.println("扩展抽象化（Refiend Abstraction）角色被访问");
+        imple.OperationImpl();
+    }
+}
